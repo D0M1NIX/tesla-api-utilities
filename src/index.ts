3371d2f09aeb10cf1d33c737988ae56808fe75cc
Chase Limpert <dellhater125@live.com>
2025-05-03 21:46:43 -0500

    try a query for Model 3

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ import * as Market from './constants/markets.const';
 import * as Region from './constants/regions.const';
 
 const teslaService = new TeslaService();
-const vehicleSpecs = {
+const vehicleSpecsMY = {
     query: {
         model: Model.MODEL_Y,
         condition: Condition.NEW,
@@ -34,9 +34,39 @@ const vehicleSpecs = {
     version: "v2"
 } as VehicleSpecs;
 
-teslaService.getNewInventoryV4(vehicleSpecs)
+const vehicleSpecsM3 = {
+    query: {
+        model: Model.MODEL_3,
+        condition: Condition.NEW,
+        options: {},
+        arrangeby: ArrangeBy.SAVINGS,
+        order: Order.DESCENDING,
+        market: Market.UNITED_STATES,
+        language: 'en',
+        super_region: 'north america',
+        PaymentType: 'cash',
+        paymentRange: 60000,
+        // zip: "54636",
+        // region: Region.WISCONSIN
+    } as TeslaInventoryQuery,
+    offset: 0,
+    //count: 24,
+    outsideOffset: 0,
+    outsideSearch: false,
+    isFalconDeliverySelectionEnabled: true,
+    version: "v2"
+} as VehicleSpecs;
+
+teslaService.getNewInventoryV4(vehicleSpecsMY)
     .then((vehicles) => {
-        console.log('Vehicles:', JSON.stringify(vehicles, null, 2));
+        console.log('Model Y\'s:', JSON.stringify(vehicles, null, 2));
+    }).catch((error) => {
+        console.error('Error fetching vehicles:', error);
+    });
+
+teslaService.getNewInventoryV4(vehicleSpecsM3)
+    .then((vehicles) => {
+        console.log('Model 3\'s:', JSON.stringify(vehicles, null, 2));
     }).catch((error) => {
         console.error('Error fetching vehicles:', error);
     });
