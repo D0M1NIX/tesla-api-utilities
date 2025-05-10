3f4422b57e8655bb7717aed83ed3dd07740e5335
Chase Limpert <dellhater125@live.com>
2025-05-10 10:45:51 -0500

    forgot model consts

--- src/constants/constants.module.ts
+++ src/constants/constants.module.ts
@@ -1,6 +1,7 @@
 export * from './arrange-by-options.const';
 export * from './conditions.const';
 export * from './markets.const';
+export * from './models.const';
 export * from './ordering.const';
 export * from './paint-options.const';
 export * from './regions.const';
