ac9409b9c5322c6513899f426340efcf4c777c4a
Chase Limpert <dellhater125@live.com>
2025-05-10 09:23:08 -0500

    move index files out to the root

---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from './src/services/tesla.service';
+export * from './src/models/models.module';
+export * from './src/constants/constants.module';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './services/tesla.service';
-export * from './models/models.module';
-export * from './constants/constants.module';
